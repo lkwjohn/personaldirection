cec3fd50bc690dec083dbe3618ddd3dd5e116ebd
lkwjohn <lkwjohn@yahoo.com>
2017-03-28 23:55:05 +0800

    a

--- test.py
+++ test.py
@@ -1,42 +1,42 @@
-import sys
-import json, urllib
-from urllib import urlencode
-import pprint
+# import sys
+# import json, urllib
+# from urllib import urlencode
+# import pprint
 
-import googlemaps
+# import googlemaps
 
-print("2222:")
-sys.stdout.flush()
-
-gmaps = googlemaps.Client(key='AIzaSyAhF49eTdOK088ldtFFkqEGt50FzWXSVoc')
-print("3333:")
-sys.stdout.flush()
-
-# # Geocoding an address
-# geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-# print("4444:")
+# print("2222:")
 # sys.stdout.flush()
 
-# # Look up an address with reverse geocoding
-# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# print("5555:")
+# gmaps = googlemaps.Client(key='AIzaSyAhF49eTdOK088ldtFFkqEGt50FzWXSVoc')
+# print("3333:")
 # sys.stdout.flush()
 
-# Request directions via public transit
+# # # Geocoding an address
+# # geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
-url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
-            ('origin', 'jurong west central 3, singapore'),
-            ('destination', 'city hall mrt, singapore'),
-            ('mode', 'transit')
- ))
+# # print("4444:")
+# # sys.stdout.flush()
 
-googleResponse = urllib.urlopen(url)
-jsonResponse = json.loads(googleResponse.read())
-pprint.pprint(jsonResponse)
+# # # Look up an address with reverse geocoding
+# # reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
 
-for i in range (0, len (jsonResponse['routes'][0]['legs'][0]['steps'])):
-    j = jsonResponse['routes'][0]['legs'][0]['steps'][i]['html_instructions'] 
-    print j                           
+# # print("5555:")
+# # sys.stdout.flush()
+
+# # Request directions via public transit
+
+# url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
+#             ('origin', 'jurong west central 3, singapore'),
+#             ('destination', 'city hall mrt, singapore'),
+#             ('mode', 'transit')
+#  ))
+
+# googleResponse = urllib.urlopen(url)
+# jsonResponse = json.loads(googleResponse.read())
+# pprint.pprint(jsonResponse)
+
+# for i in range (0, len (jsonResponse['routes'][0]['legs'][0]['steps'])):
+#     j = jsonResponse['routes'][0]['legs'][0]['steps'][i]['html_instructions'] 
+#     print j                           
 
