8ddee981203bce7f311cc0e77fd537725962d4c0
lkwjohn <lkwjohn@yahoo.com>
2017-03-28 22:18:58 +0800

    a

--- test.py
+++ test.py
@@ -1,31 +1,31 @@
-import sys
+# import sys
 
-import googlemaps
+# import googlemaps
 
-print("2222:")
-sys.stdout.flush()
-
-gmaps = googlemaps.Client(key='AIzaSyAhF49eTdOK088ldtFFkqEGt50FzWXSVoc')
-print("3333:")
-sys.stdout.flush()
-
-# # Geocoding an address
-# geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-# print("4444:")
+# print("2222:")
 # sys.stdout.flush()
 
-# # Look up an address with reverse geocoding
-# reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# print("5555:")
+# gmaps = googlemaps.Client(key='AIzaSyAhF49eTdOK088ldtFFkqEGt50FzWXSVoc')
+# print("3333:")
 # sys.stdout.flush()
 
-# Request directions via public transit
+# # # Geocoding an address
+# # geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
-directions_result = gmaps.directions("Jurong point, singapore",
-                                     "City Hall, singapore",
-                                     mode="transit")
+# # print("4444:")
+# # sys.stdout.flush()
 
-print("66666:"+directions_result)
-sys.stdout.flush()
+# # # Look up an address with reverse geocoding
+# # reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# # print("5555:")
+# # sys.stdout.flush()
+
+# # Request directions via public transit
+
+# directions_result = gmaps.directions("Jurong point, singapore",
+#                                      "City Hall, singapore",
+#                                      mode="transit")
+
+# print("66666:"+directions_result)
+# sys.stdout.flush()
