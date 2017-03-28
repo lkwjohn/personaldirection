b0586d44f3c245ecdd60a5f740520397d90d3e55
lkwjohn <lkwjohn@yahoo.com>
2017-03-28 22:16:58 +0800

    a

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-# from future.standard_library import install_aliases
-# install_aliases()
+from future.standard_library import install_aliases
+install_aliases()
 
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
