92af3b50401fc2deb2b4d6a90c3798a0c145f2e8
john <lkwjohn@yahoo.com>
2017-03-29 15:18:05 +0800

    a

--- app.py
+++ app.py
@@ -60,9 +60,14 @@ def processRequest(req):
 
     mode = parameters.get("transport_mode")
     transit_mode = parameters.get("transit_mode")
-    if mode is None:
-        mode = 'transit' #default
-    else:
+    if mode is None :
+        if transit_mode == 'bus' or transit_mode == 'train' or transit_mode == 'subway':
+            mode = 'transit' #default
+        else:
+            return makeWebhookResult('How do you want to get there by?')
+    
+
+    if mode == 'transit':
         if transit_mode is None or transit_mode == 'any':
             transit_mode = '' #any
 
